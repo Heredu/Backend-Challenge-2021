2f074e1e4f5394096b0338d1e878887f0c9dc4b8
Heredu <hernaromero@frba.utn.edu.ar>
2021-11-27 14:35:08 -0300

    check en calificacion

--- routes/api/movies.js
+++ routes/api/movies.js
@@ -2,6 +2,8 @@ const router = require('express').Router();
 
 const { pelicula } = require('../../db');
 
+const { check, validationResult } = require('express-validator');
+
 router.get('/', async (req, res)=> {
     const movies = await pelicula.findAll({
         attributes: ['imagen', 'titulo', 'fechaCreacion']
@@ -9,7 +11,14 @@ router.get('/', async (req, res)=> {
     res.json(movies)
 });
 
-router.post('/', async (req, res) => {
+router.post('/', [
+    check('calificacion', 'Calificacion debe ser del 1 al 5').isIn(['1', '2', '3', '4', '5']),
+], async (req, res) => {
+    const errors = validationResult (req);
+    if (!errors.isEmpty()) {
+        return res.status(422).json({errores: errors.array()})
+    }
+
     const movies = await pelicula.create(req.body);
     res.json(movies);
 });
